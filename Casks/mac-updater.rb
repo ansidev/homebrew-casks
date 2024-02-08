b453932f8fbea9b2b830e5d635d6cb697213ade6
ansidev <ansidev@gmail.com>
2024-02-08 09:56:56 +0700

    feat(cask): add cask mac-updater

---
+++ Casks/mac-updater.rb
@@ -0,0 +1,22 @@
+# https://www.corecode.io/macupdater/history3.html
+cask "mac-updater" do
+  version "3.2.6"
+  sha256 :no_check
+
+  url "https://www.corecode.io/downloads/macupdater_latest.dmg"
+  name "Mac Updater"
+  desc "Keep all your apps up-to-date effortlessly"
+  homepage "https://www.corecode.io/macupdater"
+
+  livecheck do
+    url "https://www.corecode.io/macupdater/history3.html"
+    strategy :page_match do |page|
+      page.scan(/(\d+\.\d+\.\d+)/i)
+          .map { |match| match&.first }
+    end
+  end
+
+  auto_updates true
+
+  app "MacUpdater.app", target: "MacUpdater.app"
+end
