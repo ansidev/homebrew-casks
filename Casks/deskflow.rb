aecac089db8c63b8e994f723d6ee716e81226046
ansidev <ansidev@gmail.com>
2025-05-31 18:45:01 +0700

    fix(github-actions): workaround for https://github.com/orgs/Homebrew/discussions/6008

--- Casks/deskflow.rb
+++ Casks/deskflow.rb
@@ -3,8 +3,10 @@ cask "deskflow" do
        intel: "x86_64"
 
   version "1.22.0"
-  sha256 arm:   "7181966f1528fca707193484a404a2626bd8778dfe4838d2740f27b7b4868804",
-         intel: "ecd1f3446d38ced3cb5fbf1651d599f3b454b3483bf0029ff9ae044f7042d3e5"
+  sha256 arm:          "7181966f1528fca707193484a404a2626bd8778dfe4838d2740f27b7b4868804",
+         intel:        "ecd1f3446d38ced3cb5fbf1651d599f3b454b3483bf0029ff9ae044f7042d3e5",
+         arm64_linux:  "0",
+         x86_64_linux: "0"
 
   url "https://github.com/deskflow/deskflow/releases/download/v#{version}/deskflow-#{version}-macos-#{arch}.dmg",
       verified: "github.com/deskflow/deskflow/"
--- Casks/peazip.rb
+++ Casks/peazip.rb
@@ -4,8 +4,10 @@ cask "peazip" do
        intel: "x86_64"
 
   version "10.4.0"
-  sha256 arm:   "546686c6f592ffa65bcedd4b47e76487e9ec980301ca4d4f0d9cae02929ee6e0",
-         intel: "0a7058c9cabc4be43809a55092c3f1f5fc3bc4bc2a3e7c275ed21a25a0c6d556"
+  sha256 arm:          "546686c6f592ffa65bcedd4b47e76487e9ec980301ca4d4f0d9cae02929ee6e0",
+         intel:        "0a7058c9cabc4be43809a55092c3f1f5fc3bc4bc2a3e7c275ed21a25a0c6d556",
+         arm64_linux:  "0",
+         x86_64_linux: "0"
 
   url "https://github.com/peazip/PeaZip/releases/download/#{version}/peazip-#{version}.DARWIN.#{arch}.dmg",
       verified: "github.com/peazip/PeaZip/"
